ca9f855ade1192ddbd1eae3d2f4ad7435a187425
Mehmet Gul <mehmetgul@gmail.com>
2019-12-02 02:58:47 -0500

    test cases finalized

--- src/test/java/vytrack/VyTrackTestCases.java
+++ src/test/java/vytrack/VyTrackTestCases.java
@@ -1,14 +1,13 @@
 package vytrack;
 
+import org.apache.poi.xssf.usermodel.XSSFPivotTable;
 import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 import utils.BrowserFactory;
 import utils.BrowserUtils;
 
@@ -19,7 +18,7 @@ public class VyTrackTestCases {
 	private WebDriver driver;
 	private WebDriverWait wait;
 
-	@BeforeMethod
+	@BeforeTest
 	public void setup() {
 		driver = BrowserFactory.getDriver("chrome");
 		//explicit wait
@@ -33,14 +32,82 @@ public class VyTrackTestCases {
 		driver.findElement(By.id("prependedInput2")).sendKeys("UserUser123", Keys.ENTER);
 		BrowserUtils.wait(5000);
 
+
+
 	}
 
-	@Test(description = "Navigate to Activities-Calendar Events-Hover on three dots for testr meeting")
+	@Test(description = "Verify that 'view', 'edit' and 'delete' options are available for each calendar event")
 	public void testCase1Run() {
 		testCase1();
 
 
 	}
+
+	@Test(description = "Verify that title column is displayed ")
+	public void testCase2run() {
+		testCase2();
+	}
+
+	@Test(description = "Verify that 'Save and Close','Save and New', and 'Save' options are available ")
+	public void testCase3Run() {
+		testCase3();
+	}
+
+	@Test(description = " Verify that 'All Calendar Events' page subtitle is displayed")
+	public void testCase4Run() {
+		testCase4();
+	}
+
+	@Test(description = "Verify that difference between end and start time is exactly 1 hour ")
+	public void testCase5Run() {
+		testCase5();
+	}
+
+	@Test(description = " Verify that end time is equals to 10:00pm when 9:00 PM start time is selected")
+	public void testCase6Run() {
+		testCase6();
+	}
+
+	@Test(description = " Verify taht 'All-Day Event' is selected and start/end time slots are not displayed")
+	public void testCase7Run() {
+		testCase7();
+
+	}
+
+	@Test(description = " Verify that Repeat checkbox is selected and that Daily checkbox selected by default")
+	public void testCase8Run() {
+
+		testCase8();
+
+	}
+
+	@Test(description = "After selecting Repeat,Repeat Every and Never button verify that summanry message is displayed ")
+	public void testCase9Run() {
+		testCase9();
+
+	}
+
+	@Test(description = " After selected Repeat, After 10 occurences Verify that the correct message is displayed")
+	public void testCase10Run() {
+
+		testCase10();
+
+	}
+
+	@Test(description = " After Selecting 'By Nov 18 2021' Verify that summary message is properly displayed")
+	public void testCase11Run() {
+
+		testCase11();
+
+	}
+
+	@Test(description = " After selecting Repeat, Weekly, Monday and Friday. Verify that summary message is correct")
+	public void testCase12Run() {
+
+		testCase12();
+
+	}
+
 
 	public void testCase1() {
 
@@ -48,6 +115,7 @@ public class VyTrackTestCases {
 		Actions a = new Actions(driver);
 
 		BrowserUtils.wait(1000);
+
 		//locating the Activites link
 		WebElement move = driver.findElement(By.xpath("//span[@class='title title-level-1'][contains(text(),'Activities')]"));
 		BrowserUtils.wait(1000);
@@ -60,7 +128,7 @@ public class VyTrackTestCases {
 		driver.findElement(By.xpath("//span[contains(text(),'Calendar Events')]")).click();
 		BrowserUtils.wait(1000);
 
-		// Narrowing the target location..
+		// Narrowing the target location to make the mouse hover easier
 		WebElement minimizeTheTarget = driver.findElement(By.xpath("//tr[13]//td[9]"));
 
 		//we are using actions class to mouse hover on the target link.
@@ -69,7 +137,7 @@ public class VyTrackTestCases {
 
 		BrowserUtils.wait(1000);
 
-		// Narrowing the target location..
+		// Narrowing the target location to make the mouse hover easier
 		WebElement minimizeTheTargetView = driver.findElement(By.xpath("//tr[13]"));
 
 		//To move our object to VIEW. we are using actions class to mouse hover on the target link.
@@ -96,11 +164,6 @@ public class VyTrackTestCases {
 		Assert.assertTrue(delete.isDisplayed());
 		BrowserUtils.wait(3000);
 
-	}
-
-	@Test
-	public void testCase2run() {
-		testCase2();
 	}
 
 	public void testCase2() {
@@ -130,7 +193,8 @@ public class VyTrackTestCases {
 			BrowserUtils.wait(1000);
 
 			WebElement name = driver.findElement(By.xpath("//div[@class='toolbar']//tr//td[1]"));
-			//finding the name which is not title and click the check box which is not title.
+
+			//finding the name which is not Title and click the check box which is not Title.
 			if (!row.get(i).getText().equalsIgnoreCase("Title")) {
 				row.get(i).click();
 
@@ -140,11 +204,6 @@ public class VyTrackTestCases {
 		String name = driver.findElement(By.xpath("//thead[@class='grid-header']//span[@class='grid-header-cell__label'][contains(text(),'Title')]")).getText();
 		Assert.assertEquals(name, "TITLE");
 		BrowserUtils.wait(8000);
-	}
-
-	@Test
-	public void testCase3Run() {
-		testCase3();
 	}
 
 	public void testCase3() {
@@ -171,7 +230,7 @@ public class VyTrackTestCases {
 		driver.findElement(By.xpath("//a[contains(@class,'btn main-group btn-primary pull-right')]")).click();
 		BrowserUtils.wait(3000);
 
-		//Exanding Sace and Close button
+		//Exanding Save and Close button
 		driver.findElement(By.xpath("//a[@class='btn-success btn dropdown-toggle']")).click();
 		BrowserUtils.wait(3000);
 
@@ -189,11 +248,6 @@ public class VyTrackTestCases {
 		Assert.assertEquals(save, "Save");
 		BrowserUtils.wait(2000);
 
-	}
-
-	@Test
-	public void testCase4Run() {
-		testCase4();
 	}
 
 	public void testCase4() {
@@ -233,13 +287,8 @@ public class VyTrackTestCases {
 		Assert.assertEquals(verifyText, "All Calendar Events");
 	}
 
-	@Test
-	public void testCase5Run() {
-		testCase5();
-	}
-
 	public void testCase5() {
-//Creating actions object to perform mouse hover
+		//Creating actions object to perform mouse hover
 		Actions a = new Actions(driver);
 
 		BrowserUtils.wait(1000);
@@ -263,11 +312,59 @@ public class VyTrackTestCases {
 		BrowserUtils.wait(3000);
 
 
-	}
+		//Clicking the first time.
+		driver.findElement(By.xpath("//input[contains(@id,'time_selector_oro_calendar_event_form_start-uid')]")).click();
+		BrowserUtils.wait(2);
+		String firstTime = "";
+		List<WebElement> ele = driver.findElements(By.xpath("//li[contains(@class,'timepicker')]"));
+		for (int i = 0; i < ele.size(); i++) {
+			//System.out.println(ele.get(i).getText());
+			BrowserUtils.wait(50);
 
-	@Test
-	public void testCase6Run() {
-		testCase6();
+			//if the first time is equal to 10am then it clicks it and exiting the loop
+			if (ele.get(i).getText().equalsIgnoreCase("9:00 AM")) {
+				firstTime = ele.get(i).getText();
+				ele.get(i).click();
+
+				break;
+			}
+		}
+
+
+		//To retrieve exact one hour difference, I will get the hour from start time and will add 1 to it
+		// and after that will compare the expected and actual.
+		int hour = 0;
+		String amPm = "",expected="";
+
+		// if the hour 2 digit (10:00 AM) using if block , and if the hour 1 digit(9:00 AM) then using else block
+		if(!firstTime.substring(1,2).equals(":")){
+			//parsing string to integer first.
+			hour = Integer.parseInt(firstTime.substring(0, 2))+1;
+
+			//Adding 1 to the hour and converting it to string again.
+			expected = hour + firstTime.substring(2);
+			System.out.println("If block run");
+		}else {
+			//parsing string to integer first.
+			hour = Integer.parseInt(firstTime.substring(0, 1))+1;
+
+			//Adding 1 to the hour and converting it to string again.
+			expected = hour + firstTime.substring(1);
+			System.out.println("else block run");
+		}
+
+
+
+		//clicking the second time value
+		driver.findElement(By.xpath("//input[contains(@id,'time_selector_oro_calendar_event_form_end-uid')]")).click();
+		BrowserUtils.wait(2);
+
+		//getting the selected value and comparing it with expected.
+		String actual=driver.findElement(By.xpath("//div[8]//li[contains(@class,'selected')]")).getText();
+		Assert.assertEquals(actual,expected,"didn't find the expected.. try more");
+
+
+
 	}
 
 	public void testCase6() {
@@ -333,12 +430,6 @@ public class VyTrackTestCases {
 
 	}
 
-	@Test
-	public void testCase7Run() {
-		testCase7();
-
-	}
-
 	public void testCase7() {
 //Creating actions object to perform mouse hover
 		Actions a = new Actions(driver);
@@ -371,13 +462,6 @@ public class VyTrackTestCases {
 		Assert.assertEquals(b, expected, "expected true");
 		System.out.println("Actual value : " + b);
 		BrowserUtils.wait(3000);
-
-	}
-
-	@Test
-	public void testCase8Run() {
-
-		testCase8();
 
 	}
 
@@ -424,12 +508,6 @@ public class VyTrackTestCases {
 		}
 		BrowserUtils.wait(3000);
 
-
-	}
-
-	@Test
-	public void testCase9Run() {
-		testCase9();
 
 	}
 
@@ -489,13 +567,6 @@ public class VyTrackTestCases {
 		BrowserUtils.wait(3000);
 	}
 
-	@Test
-	public void testCase10Run() {
-
-		testCase10();
-
-	}
-
 	public void testCase10() {
 		Actions a = new Actions(driver);
 
@@ -544,13 +615,6 @@ public class VyTrackTestCases {
 
 	}
 
-	@Test
-	public void testCase11Run() {
-
-		testCase11();
-
-	}
-
 	public void testCase11() {
 		Actions a = new Actions(driver);
 
@@ -574,45 +638,50 @@ public class VyTrackTestCases {
 		driver.findElement(By.xpath("//a[contains(@class,'btn main-group btn-primary pull-right')]")).click();
 		BrowserUtils.wait(3000);
 
-		//Select Repeat
+		//Select Repeat checkbox
 		WebElement element = driver.findElement(By.xpath("//div[@class='controls']//input[contains(@id,'recurrence-repeat-view')]"));
 		element.click();
 
 		BrowserUtils.wait(1000);
 
 		//Select By Nov 18, 2021
-		//Clicking the By radio button
+		//Clicking the "By" radio button
 		driver.findElement(By.xpath("//div[@class='recurrence-subview-control__item recurrence-subview-control__item-datetime']//label//input")).click();
 
-		//Clicking the date picker
-		WebElement narrawDatePicker = driver.findElement(By.xpath("//div[@class='recurrence-subview-control__item recurrence-subview-control__item-datetime']"));
-		narrawDatePicker.findElement(By.xpath("//input[contains(@class,'datepicker-input')]")).click();
+		//To Click datepicker, first i minimize the target location to write the locator more easy. Clicking the date picker
+		WebElement narrowDatePicker = driver.findElement(By.xpath("//div[@class='recurrence-subview-control__item recurrence-subview-control__item-datetime']"));
+		narrowDatePicker.findElement(By.xpath("//input[contains(@id,'dp')]")).click();
 
 		BrowserUtils.wait(1000);
 		//clicking the year and going down to 2021
-		narrawDatePicker.findElement(By.xpath("//select[@class='ui-datepicker-year']")).click();
-		BrowserUtils.wait(1000);
+		narrowDatePicker.findElement(By.xpath("//select[@class='ui-datepicker-year']")).click();
 
 
 		BrowserUtils.wait(2000);
+		//When a date used in <select></select> tag then we need to use the Select class.
 		Select option = new Select(driver.findElement(By.xpath("//*[@class='ui-datepicker-year']")));
+
+		// we are selecting the value which is equal to 2021
 		option.selectByValue("2021");
+
 		BrowserUtils.wait(2000);
+		//after choosing the year we are clicking the month.
 		driver.findElement(By.xpath("//*[@class='ui-datepicker-month']")).click();
 		BrowserUtils.wait(2000);
+		// Again using select class to choose the month.
 		Select option2 = new Select(driver.findElement(By.xpath("//*[@class='ui-datepicker-month']")));
+
+		//selecting the month value which is 10 (November)
 		option2.selectByValue("10");
+
+		// choosing the date 18.
 		driver.findElement(By.xpath("//*[@id='ui-datepicker-div']/table/tbody/tr[3]/td[5]/a")).click();
 		BrowserUtils.wait(2000);
+
 		WebElement print = driver.findElement(By.xpath("//*[@class='control-group recurrence-summary alert-info']"));
 		System.out.println(print.getText());
+		Assert.assertEquals(print.getText(),"Summary:\n" + "Daily every 1 day, end by Nov 18, 2021", " You are failed, Try harder!!");
 
-	}
-
-	@Test
-	public void testCase12Run() {
-
-		testCase12();
 
 	}
 
@@ -641,23 +710,36 @@ public class VyTrackTestCases {
 		driver.findElement(By.xpath("//a[contains(@class,'btn main-group btn-primary pull-right')]")).click();
 		BrowserUtils.wait(3000);
 
-		//Select Repeat
+		//Select Repeat checkbox
 		WebElement element = driver.findElement(By.xpath("//div[@class='controls']//input[contains(@id,'recurrence-repeat-view')]"));
 		element.click();
 
 		BrowserUtils.wait(1000);
 
 
-		driver.findElement(By.xpath("//*[contains(@id,'recurrence-repeats-view')]")).click();
 
-
+		//Using select class and choosing WEEKLY option from menu.
 		Select opt = new Select(driver.findElement(By.xpath("//*[contains(@class,'recurrence-repeats')]")));
 		opt.selectByValue("weekly");
 
+		BrowserUtils.wait(2000);
+		// Selecting the Monday and Friday
+		driver.findElement(By.xpath("//input[contains(@value,'monday')]")).click();
+
+		BrowserUtils.wait(1000);
+		//clicking the friday
+		driver.findElement(By.xpath("//input[contains(@value,'friday')]")).click();
+
+
+
+		//Verify the Summary
+		WebElement print = driver.findElement(By.xpath("//*[@class='control-group recurrence-summary alert-info']"));
+		System.out.println(print.getText());
+		Assert.assertEquals(print.getText(),"Summary:\n" + "Weekly every 1 week on Monday, Friday", " You are failed, Try harder!!");
 
 	}
 
-	@AfterMethod
+	@AfterTest
 	public void teardown() {
 		BrowserUtils.wait(8000);
 		driver.quit();
