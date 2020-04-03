18803754bd93064e2d61a527f8b69646a32e111f
Mehmet Gul <mehmetgul@gmail.com>
2020-04-03 00:01:07 -0400

    test commit

--- src/test/java/mehmetgul/mylib/actionclass/ActionsClass.java
+++ src/test/java/mehmetgul/mylib/actionclass/ActionsClass.java
@@ -18,9 +18,7 @@ import utils.BrowserFactory;
 public class ActionsClass {
 	static WebDriver driver;
 
-	//static WebDriver driver;
-	////static WebDriver driver;////
-	////static WebDriver driver;
+
 	public static void main(String[] args) throws InterruptedException {
 		// setting up the chrome driver
 		driver = new BrowserFactory().getDriver("chrome");
