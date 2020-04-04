362b7a4a15cd4ca2ce47aee5df96b7dd9a9cf0ba
Mehmet Gul <mehmetgul@gmail.com>
2020-04-04 01:05:17 -0400

    commit

--- src/test/java/mehmetgul/Solution.java
+++ src/test/java/mehmetgul/Solution.java
@@ -2,7 +2,7 @@ package mehmetgul;
 class Change{
 	long coin2=0;
 	long bill5=0;
-	long bill10=0; // I am from local systm
+	long bill10=0; // I am from local  mehmet
 }
 public class Solution {
 	static Change change = new Change();
