6e6ff29940e162186734004a49ab4f0da583a3b5
Mehmet Gul <mehmetgul@gmail.com>
2021-05-13 21:26:07 -0400

    mic test

---
+++ src/test/java/mehmetgul/mylib/z_mysandbox/cucumberDDT/Testaaa.java
@@ -0,0 +1,25 @@
+package mehmetgul.mylib.z_mysandbox.cucumberDDT;
+
+public class Testaaa {
+
+	public static void main(String[] args) {
+
+		double totalLocalMinutes = parseInt(local.amountused());
+		int totalSTDMinutes = std.amountUSED();
+		int totalISDMinutes = isd.amountUsed();
+
+int total=totalLocalMinutes+totalSTDMinutes+totalISDMinutes;
+if (total>input2){
+	System.out.println("{0, "+total);
+}
+else{
+	System.out.println("{1, "+total);
+}
+
+
+
+
+
+
+	}
+}
